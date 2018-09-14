0a62c45b989d41c584416e4ba57069dc1e90ac8c
bazardeh <42860792+bazardeh@users.noreply.github.com>
2018-09-14 19:26:49 +0430

    Commit WebJars Config

--- src/main/java/ir/ahmadi/springProject/controller/RestController.java
+++ src/main/java/ir/ahmadi/springProject/controller/RestController.java
@@ -5,8 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RestController {
-    @RequestMapping(path = "/test")
-    public String test() {
+    @RequestMapping(path = "/a")
+    public String a() {
+        return "/name.html";
+    }
+
+    @RequestMapping(path = "/b")
+    public String b() {
         return "/home.html";
     }
+
+    @RequestMapping(path = "/c")
+    public String c() {
+        return "/home2.html";
+    }
+
 }
