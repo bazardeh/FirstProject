9f2a876a97781196d96f2c6570ce1ec11c4f5a7c
bazardeh <42860792+bazardeh@users.noreply.github.com>
2018-11-17 22:08:56 +0330

    css alert bootstrap

--- src/main/java/ir/ahmadi/springProject/configure/WebApplicationConfig.java
+++ src/main/java/ir/ahmadi/springProject/configure/WebApplicationConfig.java
@@ -27,3 +27,11 @@ public class WebApplicationConfig implements WebMvcConfigurer {
     }
 
 }
+
+
+//<div class="alert alert-success alert-dismissible">
+//<button type="button" class="close" data-dismiss="alert">&times;</button>
+//<strong>Success!</strong> Indicates a successful or positive action.
+//</div>
+
+
